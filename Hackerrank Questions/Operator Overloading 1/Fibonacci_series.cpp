a57a54ff0f608ae8c4b21d498614b99976e4ac97
rahul2360 <rahulbansal2360@gmail.com>
2017-02-20 20:23:10 +0530

    fibonacci series question

---
+++ Hackerrank Questions/Operator Overloading 1/Fibonacci_series.cpp
@@ -0,0 +1,11 @@
+/*The fibonacci sequence begins with 0 and 1 (which are the first and the second terms
+respectively). After this, every element is the sum of the preceding elements:i.e. 0 1 2 3 5 8 13 21...
+Generate n numbers of this series by overloading a prefix ++ increment operator.
+
+Sample Input
+How many fibonaci number are to be displayed
+5
+
+Sample Output
+1 2 3 5 8*/
+
