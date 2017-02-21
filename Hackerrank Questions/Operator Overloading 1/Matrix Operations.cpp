2a1abf5aad95883a12419b16bdcb4828e679cc3a
rahul2360 <rahulbansal2360@gmail.com>
2017-02-21 20:55:12 +0530

    Matrix operation question

---
+++ Hackerrank Questions/Operator Overloading 1/Matrix Operations.cpp
@@ -0,0 +1,23 @@
+/*Write a C++ program to create a class called MATRIX using a two dimensional array of
+integers.Implement the following operations by overloading the operator == which
+checks the compatibility of the two matrices to be added and subtracted. Perform the
+addition and subtraction by overloading the operators + and ‐ respectively. Display the
+results by overloading operator <<.
+Sample Input
+2 //number of rows
+2 //number of columns
+2 3
+4 5
+2 // no of rows of second matrix
+2 //no of column of second matrix
+7 8
+9 10
+Sample Output
+Addition of matrices
+the result is
+9 11
+13 15
+subtraction of matrices
+The result is
+-5 -5
+-5 -5*/
