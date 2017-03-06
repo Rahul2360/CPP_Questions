ff87840fad3bf94b48046c1817717101e1cda6b5
Rahul2360 <rahulbansal2360@gmail.com>
2017-03-06 18:02:54 +0530

    hybrid inheritance example solution

--- Lecture Questions/Inheritence/hybrid inheritence example.cpp
+++ Lecture Questions/Inheritence/hybrid inheritence example.cpp
@@ -8,8 +8,22 @@ protected:
     int age;
     char gender;
 public:
-    void get_person_data();
-    void show_person_data();
+    void get_person_data()
+    {
+        cout << "Enter name" << endl;
+        cin.ignore();
+        cin.getline(name,20);
+        cout << "Enter age" << endl;
+        cin >> age ;
+        cout << "Enter gender" << endl;
+        cin >> gender;
+    }
+    void show_person_data()
+    {
+        cout << "Name is=" << name << endl;
+        cout << "Age is=" << age << endl;
+        cout << "Gender is=" << gender << endl;
+    }
 };
 class student:public person
 {
@@ -17,8 +31,22 @@ protected:
     int rollno;
     char course[10];
 public:
-    void get_student_data();
-    void show_student_data();
+    void get_student_data()
+    {
+        get_person_data();
+        cout << "Enter roll number" << endl;
+        cin.ignore();
+        cin >> rollno;
+        cout << "Enter course" << endl;
+        cin.ignore();
+        cin.getline(course,10);
+    }
+    void show_student_data()
+    {
+        show_person_data();
+        cout << "Roll number is=" <<  rollno << endl;
+        cout << "Course is" << course << endl;
+    }
 };
 class faculty:public person
 {
@@ -26,8 +54,20 @@ protected:
     char dept[20];
     char design[20];
 public:
-    void get_faculty_data();
-    void show_faculty_data();
+    void get_faculty_data()
+    {
+        cout << "Enter the dept" << endl;
+        cin.ignore();
+        cin.getline(dept,20);
+        cout << "Enter the design" << endl;
+        cin.ignore();
+        cin.getline(design,20);
+    }
+    void show_faculty_data()
+    {
+        cout << "Dept is" << dept << endl;
+        cout << "design is" << design << endl;
+    }
 };
 class publication:public faculty
 {
@@ -35,8 +75,23 @@ class publication:public faculty
     int no_books;
     int no_research;
 public:
-    void get_data();
-    void show_data();
+    void get_data()
+    {
+        get_faculty_data();
+        cout << "Enter the number of article" << endl;
+        cin >> no_article;
+        cout << "Enter the number of books" << endl;
+        cin >> no_books;
+        cout << "Enter the number of Research" << endl;
+        cin >> no_research;
+    }
+    void show_data()
+    {
+        show_faculty_data();
+        cout << "Article is=" << no_article << endl;
+        cout << "Books is=" << no_books << endl;
+        cout << "Research is=" << no_research << endl;
+    }
 };
 int main()
 {
