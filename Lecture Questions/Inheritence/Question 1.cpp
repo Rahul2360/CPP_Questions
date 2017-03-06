ddb7c1c1e7e229cd87f7104cad38f9c4f7486cd6
Rahul2360 <rahulbansal2360@gmail.com>
2017-03-06 18:10:03 +0530

    answer 1

--- Lecture Questions/Inheritence/Question 1.cpp
+++ Lecture Questions/Inheritence/Question 1.cpp
@@ -1,2 +1,59 @@
 // Create a class named shape that is inherited by two class circle and cone. in class circle find area.
 // In cone class find volume=1/3(pi*r*r*h)
+#include<iostream>
+#define pi 3.14
+using namespace std;
+class shape
+{
+protected:
+    int radius;
+public:
+    void get_length()
+    {
+        cout << "Enter the radius" << endl;
+        cin >> radius;
+    }
+};
+class circle:public shape
+{
+protected:
+    float a;
+public:
+    void get_circle_length()
+    {
+        get_length();
+    }
+    void area()
+    {
+        a = (float)(pi*radius*radius);
+        cout << "Area is " << a << endl;
+    }
+};
+class cone:public shape
+{
+protected:
+    int h;
+    float v;
+public:
+    void get_cone_length()
+    {
+        get_length();
+        cout << "Enter the height of cone" << endl;
+        cin >> h;
+    }
+    void volume()
+    {
+        v = (float)1/3*(pi*radius*radius*h);
+        cout << "volume is " << v << endl;
+    }
+};
+int main()
+{
+    circle ob1;
+    ob1.get_circle_length();
+    ob1.area();
+    cone ob2;
+    ob2.get_cone_length();
+    ob2.volume();
+}
+
