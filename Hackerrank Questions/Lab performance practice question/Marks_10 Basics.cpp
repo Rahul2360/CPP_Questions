0e3067b672f29fc3eb2d517ce0e467abdc8cb276
Rahul2360 <rahulbansal2360@gmail.com>
2017-03-20 21:36:51 +0530

    marks_10 basics question

--- Hackerrank Questions/Lab performance practice question/Marks_10 Basics.cpp
+++ Hackerrank Questions/Lab performance practice question/Marks_10 Basics.cpp
@@ -1,1 +1,10 @@
+Create a class named 'String' to model strings in c++.
+Take your clues from the main function and code
+stub defined in this problem.
+
+Sample Input:
+Tanmeet
+
+Sample Output:
+Tanmeet
 
