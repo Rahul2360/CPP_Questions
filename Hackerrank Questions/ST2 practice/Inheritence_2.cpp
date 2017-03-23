00ca995c206f8c504875a630951c52b00ea3692b
Rahul2360 <rahulbansal2360@gmail.com>
2017-03-23 21:17:45 +0530

    inheritence 2 executed

--- Hackerrank Questions/ST2 practice/Inheritence_2.cpp
+++ Hackerrank Questions/ST2 practice/Inheritence_2.cpp
@@ -28,28 +28,8 @@ Sample Input 1:
 0 //notes of 1
 Sample Output 1:
 Clear*/
-#include <map>
-#include <set>
-#include <list>
-#include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
 #include <string>
-#include <bitset>
-#include <cstdio>
-#include <limits>
-#include <vector>
-#include <climits>
-#include <cstring>
-#include <cstdlib>
-#include <fstream>
-#include <numeric>
-#include <sstream>
 #include <iostream>
-#include <algorithm>
-#include <unordered_map>
 using namespace std;
 class bill
 {
