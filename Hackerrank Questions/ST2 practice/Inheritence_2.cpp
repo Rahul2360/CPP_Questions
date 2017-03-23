bde962def2cc1145e927ef3b78c724587cd0e772
Rahul2360 <rahulbansal2360@gmail.com>
2017-03-23 21:09:43 +0530

    inheritence 2 question

---
+++ Hackerrank Questions/ST2 practice/Inheritence_2.cpp
@@ -0,0 +1,30 @@
+A class bill is inherited by two classes cheque, cash in protected mode.
+bill class has two members item_price,qty.
+The product of the two, 1. price of the item and 2. quantity will give us the total bill.
+class cheque again has two members cheque_no, cheque_amt.
+Write a member function to test whether the amount in the cheque clears the bill or not.
+if yes put the word "Clear" if no put the balance amount as "Balance=100" (100 is an
+example).
+Similarly class cash has 4 data members for the number of notes of 500,100,10,1.
+Total is to be calculated from the notes and again matched with the bill.
+If the bill is cleared with cash put the word "Clear" if no put the balance amount as
+"Balance=100" (100 is an example).
+Sample Input 1:
+1 // for cheque clearance
+10 //item_price
+10 //qty
+1234 //cheque_no
+80 // cheque_amt
+Sample Output 1:
+100 //bill [item_price*qty]
+Balance=20 //pending=100‐80=20
+Sample Input 1:
+2 //for cash payment
+10 //item_price
+10 //qty
+0 //notes of 500
+0 //notes of 100
+10 //notes of 10
+0 //notes of 1
+Sample Output 1:
+Clear
