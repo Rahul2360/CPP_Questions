c37716a5bbc6d307c3b6f6fdf8fb131da82f5057
Rahul2360 <rahulbansal2360@gmail.com>
2017-03-24 21:30:32 +0530

    string decrypt question

--- Hackerrank Questions/ST2 practice/String Decrypt.cpp
+++ Hackerrank Questions/ST2 practice/String Decrypt.cpp
@@ -1,1 +1,7 @@
-
+A class string1 allocates appropriate memory ﴾using new operator﴿ to a string
+passed from the main function, and then decrypt the string, by subtracting 3 to
+the ASCII value of each alphabet of the string.
+Sample Input:
+Deklodvkd
+Sample Output:
+Abhilasha
