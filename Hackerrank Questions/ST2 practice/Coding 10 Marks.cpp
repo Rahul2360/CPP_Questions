7c735399b74381b15a3ec0f50ae808e7ad44c54e
Rahul2360 <rahulbansal2360@gmail.com>
2017-03-25 21:17:50 +0530

    coding 10 marks stub

--- Hackerrank Questions/ST2 practice/Coding 10 Marks.cpp
+++ Hackerrank Questions/ST2 practice/Coding 10 Marks.cpp
@@ -1,4 +1,4 @@
-Given the following inheritance structure of classes that represents the student details,
+/*Given the following inheritance structure of classes that represents the student details,
 marks in 3 subjects and Result respectively, generate the sample output.
 Student --> Marks --> Result
 Sample Input:
@@ -10,4 +10,23 @@ Raam
 Sample Output:
 12
 Raam
-102
+102*/
+#include<iostream>
+#include<math.h>
+#include<string.h>
+using namespace std;
+
+int main() {
+    int rno,i;
+    char name[20];
+    int marks[3];
+    cin>>rno;
+    cin>>name;
+    for(i=0;i<3;i++)
+        {
+        cin>>marks[i];
+
+    }
+    Result r(rno,name,marks);
+    return 0;
+}
