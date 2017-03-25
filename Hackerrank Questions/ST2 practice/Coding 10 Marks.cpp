7517fe3cd55cba76cfa2e705912afc425c867522
Rahul2360 <rahulbansal2360@gmail.com>
2017-03-25 21:14:55 +0530

    coding 10 marks question

--- Hackerrank Questions/ST2 practice/Coding 10 Marks.cpp
+++ Hackerrank Questions/ST2 practice/Coding 10 Marks.cpp
@@ -1,1 +1,13 @@
-
+Given the following inheritance structure of classes that represents the student details,
+marks in 3 subjects and Result respectively, generate the sample output.
+Student --> Marks --> Result
+Sample Input:
+12
+Raam
+12
+34
+56
+Sample Output:
+12
+Raam
+102
