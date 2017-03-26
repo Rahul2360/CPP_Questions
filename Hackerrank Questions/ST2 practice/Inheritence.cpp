63794ab926dd3357ca1ff36f5872ceb479e4b592
Rahul2360 <rahulbansal2360@gmail.com>
2017-03-26 20:43:27 +0530

    inheritence question

--- Hackerrank Questions/ST2 practice/Inheritence.cpp
+++ Hackerrank Questions/ST2 practice/Inheritence.cpp
@@ -1,1 +1,10 @@
-
+Write a program to calculate square and cube of a number N using classes Number,Square
+and Cube.
+Constraint:
+1<=N<=1000
+Sample Input:
+2
+Sample Output:
+Square of 2 is: 4
+Cube of 2 is: 8
+YOUR ANSWER
