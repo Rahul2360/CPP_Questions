ecf0c71d71a8931dda12b5e605631b6996e462a0
Rahul2360 <rahulbansal2360@gmail.com>
2017-03-26 20:45:41 +0530

    inheritence question stub

--- Hackerrank Questions/ST2 practice/Inheritence.cpp
+++ Hackerrank Questions/ST2 practice/Inheritence.cpp
@@ -1,4 +1,4 @@
-Write a program to calculate square and cube of a number N using classes Number,Square
+/*Write a program to calculate square and cube of a number N using classes Number,Square
 and Cube.
 Constraint:
 1<=N<=1000
@@ -6,5 +6,26 @@ Sample Input:
 2
 Sample Output:
 Square of 2 is: 4
-Cube of 2 is: 8
-YOUR ANSWER
+Cube of 2 is: 8 */
+
+#include <string>
+#include <iostream>
+using namespace std;
+class Square;
+class Cube;
+class Number
+{    private:
+        int num;
+    public:
+        Number(int x);
+        int returnNumber();
+};
+
+
+
+        //sqr and cube are integer variables.
+        sqr =objS->getSquare();//objS is a pointer of Square class
+        cout << "Square of "<< objS->returnNumber() << " is: " << sqr  << endl;
+        cube=objC->getCube();//objC is a pointer of Cube class
+        cout << "Cube   of "<< objS->returnNumber() << " is: " << cube << endl;
+}
