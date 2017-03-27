d9ab21006c30aebbf084387ccd00262d8314aea1
Rahul2360 <rahulbansal2360@gmail.com>
2017-03-27 21:56:46 +0530

    student data stub

--- Hackerrank Questions/ST2 practice/Student data.cpp
+++ Hackerrank Questions/ST2 practice/Student data.cpp
@@ -1,4 +1,4 @@
-We want to use class Result that has been derived from student class in protected mode.
+/*We want to use class Result that has been derived from student class in protected mode.
 The base class (student) will contain the information: roll no and course of student.
 The derived class(result) contains the marks of three subjects and calculate the total of
 marks﴾sum of all the three subjects﴿
@@ -11,4 +11,57 @@ CSE //course of student
 Sample Output:
 23
 CSE
-135 //total marks of all the three subjects
+135 //total marks of all the three subjects*/
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+class student
+{
+int roll_no;
+protected:
+char course[10];
+public:
+student(int rno,char *c)
+{
+roll_no=rno;
+strcpy(course, c);
+}
+void show_rno();
+
+};
+class result: protected student
+
+
+int main()
+{
+int rno,m1,m2,m3;
+char course[10];
+cin>>rno;
+cin.ignore();
+cin.getline(course,10);
+cin>>m1>>m2>>m3;
+result r(rno,course,m1,m2,m3);
+r.display();
+return 0;
+}
