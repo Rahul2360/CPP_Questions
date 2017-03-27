66544c678708bdb52cdbbb9ce1e606a20e656016
Rahul2360 <rahulbansal2360@gmail.com>
2017-03-27 21:55:30 +0530

    student data question added

--- Hackerrank Questions/ST2 practice/Student data.cpp
+++ Hackerrank Questions/ST2 practice/Student data.cpp
@@ -1,1 +1,14 @@
-
+We want to use class Result that has been derived from student class in protected mode.
+The base class (student) will contain the information: roll no and course of student.
+The derived class(result) contains the marks of three subjects and calculate the total of
+marks﴾sum of all the three subjects﴿
+Sample Input:
+23 //Roll No
+CSE //course of student
+34 //marks of subject1
+45 //marks of subject2
+56 //marks of subject3
+Sample Output:
+23
+CSE
+135 //total marks of all the three subjects
