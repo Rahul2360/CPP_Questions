f2215abc9dbb2b62c4883247ff8997223fb9e300
Rahul2360 <rahulbansal2360@gmail.com>
2017-03-28 21:35:24 +0530

    Inheritence 3 question added

--- Hackerrank Questions/ST2 practice/Inheritence_3.cpp
+++ Hackerrank Questions/ST2 practice/Inheritence_3.cpp
@@ -1,1 +1,20 @@
-
+Write a program to enter basic information﴾name,empId,gender﴿ and department
+information﴾dept name,assigned work,timetaken﴿ for an employee and print the entire
+information using classes basicInfo, deptInfo and employee classes.For Wrong values print
+"Wrong Input".
+Constraint:
+timetaken can't be zero or negative.
+Sample Input:
+rahul //name
+101 //empID
+m //gender
+cse //dept name
+dl //assigned work
+45 //timetaken
+Sample Output:
+rahul
+101
+m
+cse
+dl
+4.5
