c0a3aceccbe92c34cdfe59e195472792fd76d435
Rahul2360 <rahulbansal2360@gmail.com>
2017-03-28 21:58:37 +0530

    inheritence 3 stub

--- Hackerrank Questions/ST2 practice/Inheritence_3.cpp
+++ Hackerrank Questions/ST2 practice/Inheritence_3.cpp
@@ -1,4 +1,4 @@
-Write a program to enter basic information﴾name,empId,gender﴿ and department
+/*Write a program to enter basic information﴾name,empId,gender﴿ and department
 information﴾dept name,assigned work,timetaken﴿ for an employee and print the entire
 information using classes basicInfo, deptInfo and employee classes.For Wrong values print
 "Wrong Input".
@@ -17,4 +17,31 @@ rahul
 m
 cse
 dl
-4.5
+4.5*/
+#include <string>
+#include <iostream>
+#include <math>
+using namespace std;
+class basicInfo
+{
+    protected:
+        char    *name;
+        int     empId;
+        char    gender;
+    public:
+        basicInfo(char *n,int e,char g);
+};
+class deptInfo
+{
+    protected:
+        char    *deptName;
+        char    *assignedWork;
+        int     time2complete;
+    public:
+        deptInfo(char *n,char *a,int t);
+};
+
+
+emp->printEmployeeInfo();
+return 0;
+}
