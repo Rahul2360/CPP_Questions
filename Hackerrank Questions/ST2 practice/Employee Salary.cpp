c092c0ad98f290454810e64ae9f98371722dc224
Rahul2360 <rahulbansal2360@gmail.com>
2017-03-29 21:26:04 +0530

    employee salary question

--- Hackerrank Questions/ST2 practice/Employee Salary.cpp
+++ Hackerrank Questions/ST2 practice/Employee Salary.cpp
@@ -1,1 +1,22 @@
-
+Implement a class employee that inherits two classes-(1) contract and (2) permanent.
+The base class employee contains the information of employee as: employee ID,name of the employee,designation of the
+employee.
+Derived class1 (contract﴿ contains information: no of working hours of employees, no of wages per hour and displays the salary
+of employee as the product of no of working hours and no of wages per hour
+Derived class2(permanent) contains information: basic pay, HRA, TA and calculate its DA ﴾10% of the basic salary﴿, and displays
+the calculated salary﴾sum of basic,HRA,TA,DA﴿.
+Sample Input:
+12 //employee ID
+Priyanka //employee name
+Assistant Professor //Designation of employee
+12 //no of working hours
+2000 //wages per hour
+20000 //basic salary
+800 //HRA
+400 //TA
+Sample Output:
+12
+Priyanka
+Assistant Professor
+24000 //salary of employee
+43200 //calculated salary
