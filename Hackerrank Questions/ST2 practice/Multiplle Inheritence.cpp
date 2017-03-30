9801b060fbdd739703bb87e40a26846e13b449eb
Rahul2360 <rahulbansal2360@gmail.com>
2017-03-30 21:12:11 +0530

    Multiple Inheritence stub

--- Hackerrank Questions/ST2 practice/Multiplle Inheritence.cpp
+++ Hackerrank Questions/ST2 practice/Multiplle Inheritence.cpp
@@ -1,4 +1,4 @@
-In a multiple Inheritance Problem, Base class1﴾one﴿ contains the information of first number and Base class2﴾two﴿ contains the
+/*In a multiple Inheritance Problem, Base class1﴾one﴿ contains the information of first number and Base class2﴾two﴿ contains the
 information of second number.
 Derived Class﴾last﴿ inherited from both the base classes﴾one and two﴿ gives the product of the first number and the second
 number.
@@ -8,4 +8,43 @@ Sample Input:
 Sample Output:
 33
 44
-1452 //product of two nos
+1452 //product of two nos*/
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+class one
+{
+protected:
+    int m
+public:
+    void get_m();
+};
+
+int main() {
+    last l;
+    l.get_m();
+    l.get_n();
+    l.display();
+    return 0;
+}
