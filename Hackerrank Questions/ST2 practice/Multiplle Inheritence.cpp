69ef91db0391797e3b8372d8efbce2ee3284cb26
Rahul2360 <rahulbansal2360@gmail.com>
2017-03-30 21:11:02 +0530

    Multiple Inheritence question

--- Hackerrank Questions/ST2 practice/Multiplle Inheritence.cpp
+++ Hackerrank Questions/ST2 practice/Multiplle Inheritence.cpp
@@ -1,1 +1,11 @@
-
+In a multiple Inheritance Problem, Base class1﴾one﴿ contains the information of first number and Base class2﴾two﴿ contains the
+information of second number.
+Derived Class﴾last﴿ inherited from both the base classes﴾one and two﴿ gives the product of the first number and the second
+number.
+Sample Input:
+33 //first no
+44 //second no
+Sample Output:
+33
+44
+1452 //product of two nos
