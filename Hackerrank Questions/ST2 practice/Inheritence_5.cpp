1f31354d3af8de788dbe9b7c142ee37eeca239f4
Rahul2360 <rahulbansal2360@gmail.com>
2017-04-01 23:12:49 +0530

    inheritence 5 question

--- Hackerrank Questions/ST2 practice/Inheritence_5.cpp
+++ Hackerrank Questions/ST2 practice/Inheritence_5.cpp
@@ -1,1 +1,28 @@
-
+Define a class Customer with the following specifications.
+Protected Members :
+Customer_no integer
+Customer_name char ﴾20﴿
+Qty integer
+Price ,
+Discount,
+Netprice float
+Member Functions: Public members: * A function Input﴾ ﴿ – to read data members﴾Customer_no, Customer_name, Quantity and
+Price﴿
+create Another class Seller has member function Caldiscount﴾﴿. * Caldiscount ﴾ ﴿ – To calculate Discount according to TotalPrice
+and NetPrice
+TotalPrice = Price*Qty
+TotalPrice >=50000 – Discount 25% of TotalPrice
+TotalPrice >=25000 and TotalPrice <50000 - Discount 15% of TotalPrice
+TotalPrice <250000 - Discount 10% of TotalPrice
+Netprice= TotalPrice‐Discount
+Show﴾ ﴿ – to display Customer details.
+Sample input:
+123 //customer_no
+Ravi //customer_name
+10 //quantity
+100 // Price
+Sample output:
+100
+1000 //total price=quantity*price
+100 //discount
+900 // Net‐price
