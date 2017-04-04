ffe423e4741dab29fd69eb63cbcc612fcd668d69
Rahul2360 <rahulbansal2360@gmail.com>
2017-04-04 11:29:18 +0530

    inheritence 7 question

--- Hackerrank Questions/ST2 practice/Inheritence_7.cpp
+++ Hackerrank Questions/ST2 practice/Inheritence_7.cpp
@@ -1,1 +1,42 @@
-
+Base Class name bill has three data members: item_code, price, total.
+Class cash is derived from bill having deno, value, num and cheque is also derived from bill having chec_no,bank_name as data
+members.
+The customer is having an option to buy from cheque or cash.
+Sample Input:
+//Number of items
+5
+//itemcodes and prices
+1
+10
+2
+10
+3
+10
+4
+10
+5
+10
+//Option Cheque/cash
+2
+//Num of Denominations
+3
+//1st Denomination
+Rs. 100
+//value of 1st Denomination
+Rs. 50
+//value of 2nd Denomination
+2
+//3rd Denomination
+Rs. 10
+//value of 3rd Denomination
+2
+Sample Output:
+//Total
+50
+notes=3
+100
+2
+50
+2
+10
+2
