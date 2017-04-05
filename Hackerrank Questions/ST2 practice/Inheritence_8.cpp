58ee9d0d1d079c71cd0df8209b0601205897e367
Rahul2360 <rahulbansal2360@gmail.com>
2017-04-05 22:56:05 +0530

    inheritence_8 question

--- Hackerrank Questions/ST2 practice/Inheritence_8.cpp
+++ Hackerrank Questions/ST2 practice/Inheritence_8.cpp
@@ -1,1 +1,65 @@
-
+There are three classes, your work is to display according to the choice option 1 is for ﴾address+room﴿ details, option 2 is
+for﴾house+room﴿ details for only 3 rooms not more or less than that.
+Class room: Data members are Length, Breadth, Height
+Class address: Data members are HouseNo, City, State
+Class house: Data member is HouseName
+Sample Input:
+For option 1: Address & Room details
+//Order is HouseNo, City, State
+112
+Panipat
+Haryana
+//1st Room details: Order is Length, Breadth, Height
+11
+12
+13
+//2nd Room details: Order is Length, Breadth, Height
+21
+22
+23
+//3rd Room details: Order is Length, Breadth, Height
+31
+32
+33
+State=Haryana
+Address is:
+Room Details=1
+Length=11
+Breath=12
+Height=13
+Room Details=2
+Length=21
+Breath=22
+Height=23
+Room Details=3
+Length=31
+Breath=32
+Height=33
+For option 2: House & Room details
+//HouseName
+Yellow House
+//1st Room details: Order is Length, Breadth, Height
+11
+12
+13
+//2nd Room details: Order is Length, Breadth, Height
+21
+22
+23
+//3rd Room details: Order is Length, Breadth, Height
+31
+32
+House name=Yellow House
+Address is:
+Room Details=1
+Length=11
+Breath=12
+Height=13
+Room Details=2
+Length=21
+Breath=22
+Height=23
+Room Details=3
+Length=31
+Breath=32
+Height=33
