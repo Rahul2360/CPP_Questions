0f0846475be71dea1db50b60fae426829998315f
Rahul2360 <rahulbansal2360@gmail.com>
2017-04-19 15:50:28 +0530

    max element question

---
+++ Lecture Questions/Algorithm/max-element.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    vector<int>v{3,1,-14,9,5,1};
+    vector<int>::iterator result;
+    result=max_element(v.begin(),v.end());
+    cout << "Max Value is " << *result << endl;
+}
