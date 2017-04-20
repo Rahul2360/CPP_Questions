5f276787d5f89c0286bc9dc6b8b608ddfd0f8d09
Rahul2360 <rahulbansal2360@gmail.com>
2017-04-20 13:46:22 +0530

    max and min question

---
+++ Lecture Questions/Algorithm/max and min.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int main()
+{
+    cout << "max(2,1)=" << max(2,1) << endl;
+    cout << "max(a,p)=" << max('a','p') << endl ;
+    cout << "min(3.41,1.51)=" << min(3.41,1.51) << endl;
+}
