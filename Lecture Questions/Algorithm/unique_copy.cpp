1c1d21efc3d26d8f52e183f2a3a9d9d3c3d2d19a
Rahul2360 <rahulbansal2360@gmail.com>
2017-04-20 13:41:43 +0530

    unique_copy question updated

--- Lecture Questions/Algorithm/unique_copy.cpp
+++ Lecture Questions/Algorithm/unique_copy.cpp
@@ -11,7 +11,9 @@ int main()
     int a[]={10,20,20,30,10,30,20};
     vector<int>v(7);
     vector<int>::iterator it;
+    sort(a,a+7);
     it=unique_copy(a,a+7,v.begin(),fun);
+   // v.erase(it,v.end());
     v.resize(distance(v.begin(),it));
     cout << "Vector contains" << endl;
     for(vector<int>::iterator it=v.begin();it!=v.end();it++)
